7c429a052c87caa54e92c41955fc449cc759c860
Mieszko Sabo <mieszko.sabo@icloud.com>
2023-04-09 15:54:19 +0200

    typescript eslint

--- examples/src/App.tsx
+++ examples/src/App.tsx
@@ -1,12 +1,6 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 import { hello } from "florence-state-machine";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
       <div>
